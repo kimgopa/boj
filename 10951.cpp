71a96ca7418c6251cd7e439680a37f4b5a2baee2
kimgopa <76061040+kimgopa@users.noreply.github.com>
2021-01-03 08:23:51 +0900

    A+B - 4

---
+++ 10951.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	ios_base::sync_with_stdio(false);
+	cin.tie(NULL);
+
+	int a, b;	
+	while (1)
+	{
+		cin >> a >> b;
+		if (cin.eof()) break;
+		cout << a + b << "\n";
+	}
+
+	return 0;
+}
